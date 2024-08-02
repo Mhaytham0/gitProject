20fc966e141269371c4732c95857ee137dc265d1
Nader Gamal <Nader.Jamal2501394@compit.aun.edu.eg>
2024-08-02 05:35:13 +0300

    New Branch Nader Gamal

---
+++ script.js
@@ -0,0 +1,11 @@
+//* Create Array Of Objects
+
+const Students = [
+    //* First Student 
+    {
+        name : "Nader Gamal" ,
+        age : 20 ,
+        email : "Nader.Jamal@compit.aun.edu.eg" ,
+        Specialization : "Computer Science"
+    },
+];
